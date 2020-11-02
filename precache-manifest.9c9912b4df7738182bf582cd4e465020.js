b400d4286d39a64c6fb03c0be2202740a4a661f1
mithu <mithu@gmail.com>
2020-11-02 09:56:21 +0600

    Updates

---
+++ precache-manifest.9c9912b4df7738182bf582cd4e465020.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ab2caba340bd8981a24d0060e28eab",
+    "url": "/shaiful-freelancer/index.html"
+  },
+  {
+    "revision": "9c3f1e8d14fe8048d37f",
+    "url": "/shaiful-freelancer/static/css/main.c097368b.chunk.css"
+  },
+  {
+    "revision": "8ed1005c665219d483e0",
+    "url": "/shaiful-freelancer/static/js/2.95037fca.chunk.js"
+  },
+  {
+    "revision": "7f1d5b507532efdd89fc6c6de0d248da",
+    "url": "/shaiful-freelancer/static/js/2.95037fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c3f1e8d14fe8048d37f",
+    "url": "/shaiful-freelancer/static/js/main.f9ee1f9c.chunk.js"
+  },
+  {
+    "revision": "ef52d5bedd66bd30e531",
+    "url": "/shaiful-freelancer/static/js/runtime-main.43764c5d.js"
+  },
+  {
+    "revision": "d2eb0242cf173b71fa6fd0ce07c65b2a",
+    "url": "/shaiful-freelancer/static/media/FasterOne-Regular.d2eb0242.ttf"
+  },
+  {
+    "revision": "53e7427de4aaa693950c7a9c35de80e2",
+    "url": "/shaiful-freelancer/static/media/GreatVibes-Regular.53e7427d.otf"
+  },
+  {
+    "revision": "9cce978ebf04c8bc212190c75115496e",
+    "url": "/shaiful-freelancer/static/media/Oberlo.9cce978e.png"
+  },
+  {
+    "revision": "f6ce3558870d594922c86278d4f1c06e",
+    "url": "/shaiful-freelancer/static/media/Rokkitt-Medium.f6ce3558.ttf"
+  },
+  {
+    "revision": "9b5a28bc2b14d532cde11debc45b1d64",
+    "url": "/shaiful-freelancer/static/media/amazen.9b5a28bc.png"
+  },
+  {
+    "revision": "a497d43877610108e2914fe805f60b03",
+    "url": "/shaiful-freelancer/static/media/comment.a497d438.jpg"
+  },
+  {
+    "revision": "9bc18da11deabce4f2f18c991010c2e1",
+    "url": "/shaiful-freelancer/static/media/craigslist.9bc18da1.png"
+  },
+  {
+    "revision": "055d72aa83214cc5887fd707a0cb6e18",
+    "url": "/shaiful-freelancer/static/media/ebay.055d72aa.png"
+  },
+  {
+    "revision": "1c447e0fb94f8ed8b1e756e88abbb13a",
+    "url": "/shaiful-freelancer/static/media/emailSearch.1c447e0f.png"
+  },
+  {
+    "revision": "cb1ed1df11cb2bf2ad695e41018f6071",
+    "url": "/shaiful-freelancer/static/media/etsy.cb1ed1df.png"
+  },
+  {
+    "revision": "230601ac650d1be82ff70711eff74dc2",
+    "url": "/shaiful-freelancer/static/media/fiverr.230601ac.svg"
+  },
+  {
+    "revision": "16effbf222df4a8f5f09f25d7eeeffd4",
+    "url": "/shaiful-freelancer/static/media/freelancer.16effbf2.svg"
+  },
+  {
+    "revision": "375da0bd2b5a910167a2d6392e3c46a6",
+    "url": "/shaiful-freelancer/static/media/huzz.375da0bd.png"
+  },
+  {
+    "revision": "033f438a168e9e548cab8c1bbc946868",
+    "url": "/shaiful-freelancer/static/media/imageEdit.033f438a.png"
+  },
+  {
+    "revision": "d888f01ef398f93ef11d76a0ac0fbb18",
+    "url": "/shaiful-freelancer/static/media/leadGeneration.d888f01e.png"
+  },
+  {
+    "revision": "ed92174cab89047d3c44735999595487",
+    "url": "/shaiful-freelancer/static/media/main.ed92174c.jpg"
+  },
+  {
+    "revision": "8efe251150fc4564d7e915fa11da94c6",
+    "url": "/shaiful-freelancer/static/media/myriad pro bold semiextended.8efe2511.ttf"
+  },
+  {
+    "revision": "64cf12ebfe0278529cfa5a1c7211a18e",
+    "url": "/shaiful-freelancer/static/media/myriadpro-blacksemicn.64cf12eb.otf"
+  },
+  {
+    "revision": "9c12cd3b945e33ee64ae9b1761153d68",
+    "url": "/shaiful-freelancer/static/media/myriadpro-bold.9c12cd3b.otf"
+  },
+  {
+    "revision": "4e2217c1a8309b2762499eb007d4109d",
+    "url": "/shaiful-freelancer/static/media/myriadpro-light.4e2217c1.otf"
+  },
+  {
+    "revision": "1b14fb9a0e35bddde755b1989ec4cbec",
+    "url": "/shaiful-freelancer/static/media/property.1b14fb9a.png"
+  },
+  {
+    "revision": "008e1b34b28ed127c9507afe66783cce",
+    "url": "/shaiful-freelancer/static/media/shofipy.008e1b34.png"
+  },
+  {
+    "revision": "86b514689b51625f809be89b5d85428c",
+    "url": "/shaiful-freelancer/static/media/upwork.86b51468.svg"
+  },
+  {
+    "revision": "b842f79da9940cf2782f7910275f8e05",
+    "url": "/shaiful-freelancer/static/media/wordpress.b842f79d.png"
+  },
+  {
+    "revision": "cd3ec3dcd7c9196945dbedb4b2829e2d",
+    "url": "/shaiful-freelancer/static/media/x-cart.cd3ec3dc.png"
+  }
+]);
